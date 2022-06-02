90a8461b30d66bc8882e102b89db707415934be8
leviathanbeak88 <elvisdedic@outlook.com>
2022-06-02 20:24:41 +0200

    initial client

---
+++ equity-client/src/client.rs
@@ -0,0 +1,62 @@
+use std::str::FromStr;
+
+use anyhow;
+use equity_types::{EquityAddressResponse, HealthResponse};
+use tracing::info;
+
+pub struct EquityClient {
+    url: String,
+}
+
+impl EquityClient {
+    pub fn new(url: impl AsRef<str>) -> Result<Self, anyhow::Error> {
+        Self::from_str(url.as_ref())
+    }
+
+    pub async fn health(&self) -> std::io::Result<HealthResponse> {
+        let url = surf::Url::from_str(&format!("{}/health", self.url)).unwrap();
+        info!(target = "equity-client", "URL is: {:?}", url);
+        let request: Result<HealthResponse, _> = surf::get(url).recv_json().await;
+
+        match request {
+            Ok(response) => Ok(response),
+            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
+        }
+    }
+
+    pub async fn get_account_details(
+        &self,
+        address: String,
+    ) -> std::io::Result<EquityAddressResponse> {
+        let url = surf::Url::from_str(&format!("{}/address/{}", self.url, address)).unwrap();
+        info!(target = "equity-client", "URL is: {:?}", url);
+        let query = surf::get(&url).recv_json().await;
+
+        match query {
+            Ok(response) => Ok(response),
+            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
+        }
+    }
+}
+
+impl FromStr for EquityClient {
+    type Err = anyhow::Error;
+
+    fn from_str(str: &str) -> Result<Self, Self::Err> {
+        Ok(Self {
+            url: str.to_owned(),
+        })
+    }
+}
+
+impl<S> From<S> for EquityClient
+where
+    S: Into<std::net::SocketAddr>,
+{
+    fn from(socket: S) -> Self {
+        format!("http://{}", socket.into())
+            .as_str()
+            .parse()
+            .unwrap()
+    }
+}
