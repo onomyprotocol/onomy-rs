a48c440b5e8f1d651360b42955ad64b92b587210
leviathanbeak88 <elvisdedic@outlook.com>
2022-06-02 20:19:01 +0200

    add initial InMemory storage implementation

---
+++ equity-storage/src/in_memory.rs
@@ -0,0 +1,27 @@
+use std::{collections::HashMap, sync::Mutex};
+
+use crate::EquityStorage;
+
+#[derive(Debug, Default)]
+pub struct InMemoryDb {
+    data_holder: Mutex<HashMap<Vec<u8>, Vec<u8>>>,
+}
+
+impl EquityStorage for InMemoryDb {
+    fn get(&self, key: &[u8]) -> crate::DatabaseResult<Option<Vec<u8>>> {
+        Ok(self
+            .data_holder
+            .lock()
+            .expect("Lock is poisoned")
+            .get(key)
+            .cloned())
+    }
+
+    fn insert(&self, key: Vec<u8>, value: Vec<u8>) -> crate::DatabaseResult<Option<Vec<u8>>> {
+        Ok(self
+            .data_holder
+            .lock()
+            .expect("Lock is Poisoned")
+            .insert(key, value))
+    }
+}
