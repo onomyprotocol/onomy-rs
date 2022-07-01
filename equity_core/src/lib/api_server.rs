b60c2d0e4e0be1925405f42930efc1bc1cea9ba2
Charles Dusek <dusek@onomy.io>
2022-07-01 10:45:56 -0500

    Added signature verification to server side

--- equity_core/src/lib/api_server.rs
+++ equity_core/src/lib/api_server.rs
@@ -73,13 +73,18 @@ async fn transaction(
 
     info!(target = "equity-core", "Transaction API");
 
-    let key:&[u8] = payload.body.public_key.as_bytes();
+    let public_key:&[u8] = payload.body.public_key.as_bytes();
 
-    // Mapping [public_key -> nonce] 
+    // First rule: Transaction Nonce > All Previous Nonce 
 
     let mut previous_nonce: u64 = 0;
 
-    match state.get(key) {
+    // Check database if Mapping [public_key -> nonce] exists
+    // If value exists 
+    // THEN previous_nonce == value
+    // ELSE previous_nonce == 0
+
+    match state.get(public_key) {
         Ok(Some(value)) => {
             previous_nonce = value;
             info!("nonce: {}", previous_nonce);
@@ -92,22 +97,42 @@ async fn transaction(
         }
     }
 
-
+    // IF tx nonce > previous_nonce
+    // THEN Verify signature
+    // ELSE Send Response with Success: False and previous nonce
 
     if payload.body.nonce > previous_nonce {
-        Ok(Borsh(PostTransactionResponse { 
-            success: true, 
-            nonce: payload.body.nonce  
-        }))
+
+        let message_string = serde_json::to_string(&payload.body).unwrap();
+
+        let mut digest: Sha512 = Sha512::new();
+        digest.update(message_string);
+
+        let digest_string: String = format!("{:X}", digest.clone().finalize());
+
+        let verified = payload.body.public_key.verify(&payload.signature, &digest_string.as_bytes());
+        
+        // Signature verified? 
+        match verified {
+            Ok(_o) => {
+                Ok(Borsh(PostTransactionResponse { 
+                    success: true, 
+                    nonce: payload.body.nonce  
+                }))
+            }
+            Err(_e) => {
+                Ok(Borsh(PostTransactionResponse { 
+                    success: false, 
+                    nonce: payload.body.nonce  
+                }))
+            }
+        }  
     } else {
         Ok(Borsh(PostTransactionResponse { 
             success: false, 
             nonce: previous_nonce  
         }))
     }
-
-    // payload.body.public_key.verify(&payload.signature, &payload.body);
-    
 }
 
 // TODO should we use some binary instead of a path?
