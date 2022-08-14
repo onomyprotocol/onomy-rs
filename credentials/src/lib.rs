7101728f422a33bf4b1a58117081cffaafd0b336
Charles Dusek <dusek@onomy.io>
2022-08-14 15:31:05 -0500

    Saving progress

--- credentials/src/lib.rs
+++ credentials/src/lib.rs
@@ -7,106 +7,56 @@ use equity_types::MsgType;
 // The HashMap
 
 #[derive(Debug, Clone)]
-    pub struct Credentials {
+pub struct Credentials {
+    public_key: VerificationKey,
     sender: mpsc::Sender<Command>
 }
 
-impl Brb {
+impl Credentials {
     pub fn new() -> Brb {
         let (tx, mut rx) = mpsc::channel(1000);
+
+        let signer = Internal::new();
 
         tokio::spawn(async move
             {
                 while let Some(cmd) = rx.recv().await {
-                    match cmd {
-                        Command::Sign { key, resp } => {
-                            let response = brb_map.get(&key);
+                    
+                    signer = signer.clone();
 
-                            match response {
-                                Some(sender) => {
-                                    resp.send(Some(sender.clone())).unwrap()
-                                },
-                                None => resp.send(None).unwrap()
-                            };
+                    match cmd {
+                        Command::Sign { msg, resp } => {
+                            
+                            // Hash + Signature operation may be considered blocking
+
+                            let mut digest: Sha512 = Sha512::new();
+                            
+                            digest.update(msg);
+
+                            let digest_string: String = format!("{:X}", digest.finalize());
+
+                            let signature = signer.private_key.sign(digest_string.as_bytes());
+
+                            (digest_string, signature)
+
+
+                            resp.send(Some(sender.clone())).unwrap();
                         }
                     }
                 }
             }
         );
-        Brb {
+        
+        Self {
             sender: tx
         }
     }
 
-    async fn get(&self, hash: &String) -> Option<mpsc::Sender<BrbMsg>> {
+    async fn sign(&self, msg: &String) -> Option<(String, Signature)> {
         let (tx, rx) = oneshot::channel();
-        self.sender.send(BrbCommand::Get { key: hash.clone(), resp: tx }).await.unwrap();
+        self.sender.send(Command::Sign { msg: msg, resp: tx }).await.unwrap();
         rx.await.unwrap()
     }
-
-    // All BRB broadcast messages are either initiated or received.
-    // Initiation is prompted by out of network messages (client / new validator) or enabled consensus condition.
-    // All in-network messages are Received as part of Brb Broadcast
-    pub async fn initiate (&self, hash: String, peer: VerificationKey, msg: MsgType) {
-        // First need to check if there is already an initiated BRB instance with this same hash
-        if let Some(brb_sender) = self.get(&hash).await {
-            // BRB manager exists then treat as Echo
-            // Need to define below how to use Echo before completing this part.
-            brb_sender.send(BrbMsg::Echo{
-                hash: hash.clone(),
-                peer,
-                msg
-            }).await.unwrap()
-        }
-        
-        let (brb_tx, mut brb_rx) = mpsc::channel(1000);
-        let (brb_one_tx, brb_one_rx) = oneshot::channel();
-        
-        tokio::spawn(async move
-            {
-                while let Some(brb_msg) = brb_rx.recv().await {
-                    match brb_msg {
-                        BrbMsg::Init { hash, peer, msg } => {
-                            
-                        }
-                        BrbMsg::Echo { hash, peer, msg } => {
-                            
-                        }
-                        BrbMsg::Ready { hash } => {
-                            
-                        }
-                    }
-                }
-
-                brb_one_tx.send(true).unwrap();
-            }
-        );
-
-        let (one_tx, one_rx) = oneshot::channel();
-
-        self.sender.send(BrbCommand::Set
-            {
-                key: hash, 
-                val: brb_tx, 
-                resp: one_tx
-            }
-        ).await.unwrap();
-
-        match one_rx.await {
-            Ok(v) => println!("got = {:?}", v),
-            Err(_) => println!("the sender dropped"),
-        }
-
-        match brb_one_rx.await {
-            Ok(v) => println!("got = {:?}", v),
-            Err(_) => println!("the sender dropped"),
-        }
-    }
-
-    pub fn receive () {
-
-    }
-
 }
 
 /// Multiple different commands are multiplexed over a single channel.
@@ -115,8 +65,8 @@ impl Brb {
 #[derive(Debug)]
 enum Command {
     Sign {
-        key: String,
-        resp: Responder<Option<mpsc::Sender<BrbMsg>>>,
+        msg: String,
+        resp: Responder<Option<mpsc::Sender<Response>>>,
     }
 }
 
@@ -137,6 +87,16 @@ impl Internal {
             public_key: vk,
             nonce: 1,
         }
+    }
+
+    fn sign()
+}
+
+#[derive(Debug)]
+enum Response {
+    Sign {
+        hash: String,
+        signature: Signature
     }
 }
 
