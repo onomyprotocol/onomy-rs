1499a41b63b01ca28ff019edb73ded8a643be10a
Charles Dusek <38732970+cgdusek@users.noreply.github.com>
2022-09-11 21:56:45 -0500

    Saving progress

--- equity_consensus/src/broadcast.rs
+++ equity_consensus/src/broadcast.rs
@@ -74,125 +74,83 @@ impl Brb {
         false
     }
 
-    // All BRB broadcast messages are either initiated or received.
-    // Initiation is prompted by out of network messages (client / new validator) or enabled consensus condition.
-    // All in-network messages are Received as part of Brb Broadcast
-    pub async fn initiate (&self, peers: PeerMap, hash: &String, public_key: &VerificationKey, broadcast_msg: &BroadcastMsg) {
-        // First need to check if there is already an initiated BRB instance with this same hash
-        /*
-        if let Some(brb_sender) = self.get(&hash).await {
-            // BRB manager exists then treat as Echo
-            // Need to define below how to use Echo before completing this part.
-            brb_sender.send(
-                BrbMsg::Echo{
-                    public_key: public_key.clone(),
-                    broadcast_msg: broadcast_msg.clone()
+    async fn echo(&self, internal: &BrbInternal, public_key: &VerificationKey) {
+        if let Some(tally_len) = internal.update_tally(&"Echo".into(), &public_key) {
+            if internal_handler.ctl == "Echo".to_string() {
+                // If BRB in ctl = "Echo" and cardinality of internal.echo = (n+t)/2 broadcast 
+                // Then broadcast "Ready"
+                
+                // Cardinality of peers is going to get contentious - need to create manager
+                if internal.echo.len() > peers.cardinality()/2 {
+                    // Broadcast Ready
+                    peers.broadcast(
+                    Broadcast::Ready {
+                        hash: hash_spawn.clone()
+                    }).await;
+                    
+                    internal.ctl = "Ready".to_string();
                 }
-            ).await.unwrap()
+            }
         }
-        
-        */
-        
+    }
+
+    // All broadcasts are initiated whether originating from self or external
+    pub async fn initiate (&self, peers: PeerMap, hash: &String, public_key: &VerificationKey, broadcast_msg: &BroadcastMsg) {
         let (brb_tx, mut brb_rx) = mpsc::channel(1000);
         let (brb_one_tx, brb_one_rx) = oneshot::channel();
         let hash_spawn = hash.clone();
         let broadcast_msg = broadcast_msg.clone();
         let self_internal = self.clone();
 
+        let internal = Arc::new(Mutex::new(
+            BrbInternal {
+                hash: hash_spawn,
+                ctl: "Init".to_string(),
+                msg: broadcast_msg,
+                init: false,
+                tally: HashMap::new(),
+                commit: false
+        }));
+
         tokio::spawn(async move
             {
-                let mut internal = Arc::new(Mutex::new(BrbInternal {
-                    hash: hash_spawn.clone(),
-                    ctl: "Echo".to_string(),
-                    msg: broadcast_msg,
-                    init: true,
-                    tally: HashMap::new(),
-                    commit: false
-                }));
-
                 while let Some(brb_msg) = brb_rx.recv().await {
-                    let internal_handler = internal.clone();
+                    let internal_handler = internal.lock().unwrap();
+                    
+                    match brb_msg {
+                        BrbMsg::Init { public_key, broadcast_msg } => {
+                            
 
-                    tokio::spawn(async move
-                        {
-                            match brb_msg {
-                                BrbMsg::Init { public_key, broadcast_msg } => {
-                                    let internal_handler = internal_handler.lock().unwrap();
+                            if internal_handler.ctl == "Echo".to_string() {
+                                self_internal.echo(&internal_handler, &public_key);
+                            }   
+                        }
+                        BrbMsg::Echo { public_key, broadcast_msg } => {
+                            // Receiving echo before init, Timeout
+                            // Assumes network routing through another peer is always
+                            // slower than direct P2P connection.  Not sure if this
+                            // holds
+                            if internal_handler.ctl == "Init".to_string() {
+                                internal_handler.ctl = "Timeout".to_string();
 
-                                    // First need to check if there is already a timeout
-                                    // Timeout caused by receiving Echo before Init msg
-                                    if internal_handler.ctl == "Timeout".to_string() {
-                                        return
-                                    }
-                                    // If Init did not initiate the BRB then it is a timeout.  Within the strict model
-                                    // of communication required by this protocol.  Is that too strict?
-    
-                                    // Check if this broadcast has already been initialized
-                                    // If broadcast has been initialized, then treat init as echo
-                                    if let Some(_brb_sender) = self_internal.get(&hash_spawn.clone()).await {
-                                        // Add public key to hashset
-                                        // This results in bool, do we want to punish for duplicate?
-                                        let _nonexists = internal_handler.tally(echo.insert(key_to_string(&public_key).unwrap());
-                                    }
-                                }
-                                // Will not reach this code unless the BRB has been initiated.
-                                BrbMsg::Echo { public_key, broadcast_msg } => {
-                                    
-                                    let internal_handler = internal_handler.lock().unwrap();
-
-                                    if let Some(tally_len) = internal_handler.update_tally(&"Echo".into(), &public_key) {
-                                        if internal_handler.ctl == "Echo".to_string() {
-                                            // If BRB in ctl = "Echo" and cardinality of internal.echo = (n+t)/2 broadcast 
-                                            // Then broadcast "Ready"
-                                            
-                                            // Cardinality of peers is going to get contentious - need to create manager
-                                            if internal.echo.len() > peers.cardinality()/2 {
-                                                // Broadcast Ready
-                                                peers.broadcast(
-                                                Broadcast::Ready {
-                                                    hash: hash_spawn.clone()
-                                                }).await;
-                                                
-                                                internal.ctl = "Ready".to_string();
-                                            }
-                                        }
-                                    }
-
-                                    if internal.ctl == "Ready".to_string() {
-                                        // Step 2 (Ready) Bracha BRB: cardinality of internal.
-                                    }
-    
-        /*
-        } else {
-            // If BRB instance does not exist for msg.hash then Timeout
-    
-            // Add public key to echo hashset regardless of ctl
-            // This results in bool, do we want to punish for duplicate echo?
-            let _nonexists = internal.echo.insert(key_to_string(&public_key).unwrap());
-    
-            if internal.ctl == "Timeout" {
-                
-                internal.ctl = "Timeout".to_string();
-    
-                // BRB manager does not exist send timeout
-                // Broadcast timeout
-                peers.broadcast(
-                    Broadcast::Timeout {
-                        hash: hash_spawn.clone()
-                }).await;
-            }
-        }
-        */
-                                    
-                                }
-                                BrbMsg::Ready { hash } => {
-                                    
-                                }
-                                BrbMsg::Timeout { hash } => {
-    
-                                }
+                                // Broadcast timeout
+                                peers.broadcast(
+                                    Broadcast::Timeout {
+                                        hash: hash_spawn.clone()
+                                }).await;
                             }
-                        });
+
+                            if internal_handler.ctl == "Ready".to_string() {
+                                // Step 2 (Ready) Bracha BRB: cardinality of internal.
+                            }
+                        }
+                        BrbMsg::Ready { hash } => {
+                            
+                        }
+                        BrbMsg::Timeout { hash } => {
+
+                        }
+                    }  
                 }
                 brb_one_tx.send(true).unwrap();
             });
